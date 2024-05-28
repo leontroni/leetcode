8a5625f699995759fd7f1f7e22dab4d78f1c3bfd
leontroni <153965703+leontroni@users.noreply.github.com>
2024-05-29 00:04:08 +0200

    Time: 0 ms (100.00%) | Memory: 11.5 MB (42.58%) - LeetSync

---
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,30 @@
+class Solution { // Minimum Distance Between BST Nodes
+public:
+    // Time Complexity: O(n), Space Complexity: O(n)
+     void diff(TreeNode* root, vector<int>& arr)
+     {
+         if (root == NULL)
+         {
+             return;
+         }
+         diff(root->left, arr);
+         arr.push_back(root->val);
+         diff(root->right, arr);
+     }
+     int minDiffInBST(TreeNode* root)
+     {
+         vector<int>arr;
+         diff(root, arr);
+         int min1, min2 = INT_MAX;
+         for (int i = 0; i < arr.size() - 1; i++)
+         {
+             min1 = arr[i + 1] - arr[i];
+             if (min1 <= min2)
+             {
+                 min2 = min1;
+             }
+         }
+         return min2;
+
+     }
+};
